d202d2aba8faac305bbb6a56d93c52d304a01f90
mertunaldi24 <mert@liquisto.com>
2023-08-15 17:54:00 +0200

    Latest

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import csv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import os
+import requests
+from bs4 import BeautifulSoup
 
 
-
-
-
-
-
-
-
-print ( " Mert " )
-
-
-print ( " Mert " )
+# Read CSV file
+with open('products.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        vendor = row['vendor']
+        barcode = row['barcode']
+        
+        # Combine vendor and barcode to create search query
+        search_query = f"{vendor} {barcode}"
+        
+        # Construct URL and send request
+        search_url = f"https://de.wiaautomation.com/q={search_query}"
+        response = requests.get(search_url)
+        html_content = response.text
+        
+        # Parse HTML content
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        # Check if product found in search results
+        not_found_element = soup.find('div', class_='not-found-message')
+        if not_found_element:
+            print(f"Product: {vendor} - {barcode} not found")
+            continue
+        
+        # Extract price information based on HTML structure
+        price_element = soup.find('span', class_='price')
+        if price_element:
+            price = price_element.text
+            print(f"Product: {vendor} - {barcode}, Price: {price}")
+        else:
+            print(f"Price not found for {vendor} - {barcode}")
